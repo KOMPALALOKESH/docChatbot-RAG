82da45f137a384f561b99d0a6075ee7169c5926d
KOMPALALOKESH <83068533+KOMPALALOKESH@users.noreply.github.com>
2024-03-10 19:03:08 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,35 @@
+from llama_index.llms.huggingface import HuggingFaceLLM
+from llama_index.core import PromptTemplate
+from llama_index.embeddings import HuggingFaceEmbedding
+from llama_index.core import VectorStoreIndex, ServiceContext
+
+system_prompt = "You are a Q&A assistant. Your goal is to answer questions as accurately as possible based on the instructions and context provided."
+# This will wrap the default prompts that are internal to llama-index
+query_wrapper_prompt = PromptTemplate("<|USER|>{query_str}<|ASSISTANT|>")
+
+llm = HuggingFaceLLM(
+    context_window=2048,
+    max_new_tokens=256,
+    generate_kwargs={"temperature": 0.0, "do_sample": False},
+    system_prompt=system_prompt,
+    query_wrapper_prompt=query_wrapper_prompt,
+    tokenizer_name="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    model_name="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    device_map="cuda",
+    # uncomment this if using CUDA to reduce memory usage
+    model_kwargs={"torch_dtype": torch.bfloat16}
+)
+
+# loads BAAI/bge-small-en-v1.5
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+# service context
+service_context = ServiceContext.from_defaults(
+    chunk_size=1024,
+    llm=llm,
+    embed_model=embed_model
+)
+
+# indexing
+index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+# query engine
+query_engine = index.as_query_engine()
