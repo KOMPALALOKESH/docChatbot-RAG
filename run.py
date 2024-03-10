2f7ade084209507c6d380364e7bb420e30e94a3f
KOMPALALOKESH <83068533+KOMPALALOKESH@users.noreply.github.com>
2024-03-10 19:37:16 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from llama_index.core import SimpleDirectoryReader
 from constants import title, description, examples
-from model import query_engine
+from model import query_engine, service_context
 import gradio as gr
 import os
 import shutil
@@ -13,7 +13,7 @@ def move_pdf_files():
 
     for pdf_file in pdf_files:
         src_path = os.path.join("/content", pdf_file)
-        dest_path = os.path.join("/content/Data", pdf_file)
+        dest_path = os.path.join("/content/assignment/Data", pdf_file)
         shutil.move(src_path, dest_path)
     return "success"
 
@@ -22,14 +22,20 @@ def generate(input, history):
   response = query_engine.query(input)
   return str(response)
 
-# read directory
-documents = SimpleDirectoryReader("/content/Data").load_data()
-
 # gradio UI
 with gr.Blocks() as demo:
     # file input
     file_output = gr.File()
     upload_button = gr.UploadButton("Click to Upload a File", file_types=["pdf"], file_count="multiple")
+    move_pdf_files()
+    # read directory
+    documents = SimpleDirectoryReader("/content/assignment/Data").load_data()
+
+    # indexing
+    index = VectorStoreIndex.from_documents(documents, service_context=service_context)
+    # query engine
+    query_engine = index.as_query_engine()
+    
     # chat UI
     with gr.Blocks(): 
         gr.ChatInterface(
@@ -38,5 +44,5 @@ with gr.Blocks() as demo:
             description=description,
             examples=examples)
 
-if __name__=="__main__":
+if __name__=="__main__":    
     demo.launch(debug=True, share=True)
