22c6d1188da30e86151f68685a2ed72195605b85
KOMPALALOKESH <83068533+KOMPALALOKESH@users.noreply.github.com>
2024-03-10 19:21:16 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from llama_index.core import SimpleDirectoryReader
+from constants import title, description, examples
+from model import query_engine
+import gradio as gr
 import os
 import shutil
 
 # move files to "Data" folder
-def move_pdf_files(source_dir, destination_dir):
+def move_pdf_files():
     os.makedirs("Data", exist_ok=True)
 
     pdf_files = [f for f in os.listdir(source_dir) if f.endswith(".pdf")]
@@ -19,4 +22,21 @@ def generate(input, history):
   response = query_engine.query(input)
   return str(response)
 
+# read directory
 documents = SimpleDirectoryReader("/content/Data").load_data()
+
+# gradio UI
+with gr.Blocks() as demo:
+    # file input
+    file_output = gr.File()
+    upload_button = gr.UploadButton("Click to Upload a File", file_types=["pdf"], file_count="multiple")
+    # chat UI
+    with gr.Blocks(): 
+        gr.ChatInterface(
+            fn=generate,
+            title=title,
+            description=description,
+            examples=examples)
+
+if __name__=="__main__":
+    demo.launch(debug=True, share=True)
