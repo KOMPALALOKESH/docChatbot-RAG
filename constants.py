f9ea9939cdd6059d8a1bb9ce22141627928dd752
KOMPALALOKESH <83068533+KOMPALALOKESH@users.noreply.github.com>
2024-03-10 19:12:36 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,13 @@
+title = "Document Retrieval System"
+
+description = '''Need a needle in a haystack of files? TinyLlama's got you covered! It's a document retrieval whiz, effortlessly sniffing out the right info for you. 🚀🚀
+This pint-sized powerhouse is packing 1.1 billion parameters and has been trained on a whopping 3 trillion tokens! That's like reading the entire internet three times over...
+and then some! Say goodbye to endless scrolling and hello to instant insights!⚡️⚡️ How does it work? TinyLlama understands language like a pro, so it can match your search queries to
+the most relevant docs, even if they're hidden in the deepest archives 🪄 . No more digging through dusty digital boxes!Plus, it's super efficient, saving you time and energy⏳.
+That's more time for brainstorming and less time for file hunting! ️‍♀️'''
+
+examples = ["What is the main topic of the document?",
+            "What are the key points or arguments presented in the document?",
+            "What are the strengths and weaknesses of the document in terms of its structure, clarity, and evidence?",
+            "What are the main conclusions or takeaways from the document?",
+            "What are some potential applications of the information in the document, according to its content?"]
